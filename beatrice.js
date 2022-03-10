6b24cb893fc3e74e7a74a289fc797197b41cb0e1
dotan-harel <frida500@gmail.com>
2022-03-10 23:03:49 +0200

    add commands

--- beatrice.js
+++ beatrice.js
@@ -1,28 +1,39 @@
 require('dotenv').config()
 const config = require('./config')
+const { clientToken, botChannel, clientId, guildId } = require('./config')
 const { Client, Intents } = require('discord.js')
 const myIntents = new Intents()
 myIntents.add(Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.DIRECT_MESSAGES)
 
 const client = new Client({ intents: myIntents })
 
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`)
-  NOTIFY_CHANNEL = client.channels.fetch(config['Bot_Channel'])
+client.once('ready', () => {
+  console.log('Ready!')
+  // const channel = client.channels.cache.get(botChannel);
 })
 
-client.on('message', async message => {
-{
-  var input = message.content.toLowerCase().replace(/ +(?= )/g,'')
-  if (input === "!ping")
-  {
-    message.reply("Pong! :3")
-  }
-  else if (input === "!calendar")
-  {
-    
-  }
-}
-})
+client.on('interactionCreate', async interaction => {
+  if (!interaction.isCommand()) return;
 
-client.login(config['ClientToken'])
+  const { commandName } = interaction;
+
+  if (commandName === 'ping') {
+    await interaction.reply('Pong!');
+  }
+});
+
+// client.on('message', async message => {
+// {
+//   const input = message.content
+//   if (input === "!ping")
+//   {
+//     await message.reply("Pong! :3")
+//   }
+//   else if (input === "!calendar")
+//   {
+//     notify.send( { embeds: [parseCalendar] })
+//   }
+// }
+// })
+
+client.login(clientToken).then(() => console.log(`Logged in as ${client.user.tag}!`))
