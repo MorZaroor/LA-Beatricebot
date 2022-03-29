fd0a8887e1c49a843e2c7994aa25a7a78bb509c7
Mor Zaroor <morza9696@gmail.com>
2022-03-29 19:32:27 +0300

    added next hour calendar

--- beatrice.js
+++ beatrice.js
@@ -1,30 +1,27 @@
 require('dotenv').config()
-const config = require('./config')
 const { clientToken, botChannel, clientId, guildId } = require('./config')
-const { parseCalendar } = require('./calendar')
+const { parseCalendar, parseDailyCalendar } = require('./calendar')
 const { Client, Intents } = require('discord.js')
 const myIntents = new Intents()
-
-myIntents.add(Intents.FLAGS.GUILDS)
+myIntents.add(Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.DIRECT_MESSAGES, Intents.FLAGS.GUILDS)
 
 const client = new Client({ intents: myIntents })
 
-client.once('ready', () => {
-  console.log('Ready!')
-  // const channel = client.channels.cache.get(botChannel);
-})
-
 client.on('interactionCreate', async interaction => {
+  console.log("peepeepoopoo")
   if (!interaction.isCommand()) return;
 
   const { commandName } = interaction;
 
   if (commandName === 'ping') {
-    await interaction.reply('Pong!');
+    await interaction.reply('Pong!')
+  }
+})
+
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`)
-  NOTIFY_CHANNEL = client.channels.fetch(config['BotChannel'])
+  NOTIFY_CHANNEL = client.channels.fetch(botChannel)
   setInterval(() => {
     var cet = new Date().toLocaleString("en-US", { timeZone: "CET" })
     let cet_obj = new Date(cet)
@@ -43,10 +40,15 @@ client.on('message', async message => {
   {
     await parseCalendar(message)
   }
+  else if (input === "!dailycalendar")
+  {
+    await parseDailyCalendar(message)
+  }
   else if (input === "!clear")
   {
     message.guild.channels.cache.find(i => i.name === 'bot-test').bulkDelete(100, true)
   }
-});
+}
+})
 
 client.login(clientToken).then(() => console.log(`Logged in as ${client.user.tag}!`))
--- calendar.js
+++ calendar.js
@@ -21,13 +21,61 @@ const parseCalendar = async (message) => {
     var times = response.data.substring(response.data.lastIndexOf("var calendar_data=") + 18, response.data.lastIndexOf("var calendar_events=")).replace(';','')
     var parsed = JSON.parse(times)
 
+    const calendarEmbed = new MessageEmbed()
+    .setColor('#0099ff')
+    .setTitle("Events starting within the next hour")
+    .setAuthor({ name: 'Beatrice the Seductress', iconURL: 'https://cdn.discordapp.com/avatars/951237146386251886/6365bdb68ecf9847880ea6e20e6ba05c.webp' })
+    .setThumbnail('https://lostarkcodex.com/images/icon_calendar_event_4.webp')
+    .setImage('https://lostarkcodex.com/images/icon_calendar_event_4.webp')
+    .setTimestamp()
+    .setFooter({ text: 'peepeepoopoo', iconURL: 'https://cdn.discordapp.com/avatars/951237146386251886/6365bdb68ecf9847880ea6e20e6ba05c.webp' })
+
     Object.keys(event_types.calendar_categories).forEach(async (category) => {
         var events = parsed[category][month][day]
         if (typeof events === 'undefined')
         {
             return
         }
-        const categoryEmbed = new MessageEmbed()
+
+
+        Object.keys(events).forEach((gearScore) => {
+            Object.keys(events[gearScore]).forEach((eventID) => {
+                console.log(calendarEmbed)
+                if (typeof events[gearScore][eventID] !== 'undefined' && events[gearScore][eventID])
+                {
+                    Object.keys(events[gearScore][eventID]).forEach((event_time) => {
+                        let event_hour = events[gearScore][eventID][event_time].split(':')[0]
+                        let event_minute = events[gearScore][eventID][event_time].split(':')[1]
+                        let event_hdiff = (event_hour - date_ob.getHours()) * 60
+                        let event_mdiff = (event_minute - date_ob.getMinutes()) + event_hdiff
+                        if (event_mdiff >= 0 && event_mdiff <= 60)
+                        {
+                            calendarEmbed.addField(event_types.calendar_events[eventID][0], events[gearScore][eventID][event_time].replace(/\,/g,'\n'), false)
+                        }
+                    })
+                }
+            })
+        })
+    })
+    await message.guild.channels.cache.find(i => i.name === 'bot-test').send({ embeds: [calendarEmbed] })
+}
+
+const parseDailyCalendar = async (message) => {
+    var date_ob = new Date()
+    var day = ('0' + date_ob.getDate()).slice(-2)
+    var month = date_ob.getMonth() + 1
+
+    var response = await getHTML()
+    var times = response.data.substring(response.data.lastIndexOf("var calendar_data=") + 18, response.data.lastIndexOf("var calendar_events=")).replace(';','')
+    var parsed = JSON.parse(times)
+
+    Object.keys(event_types.calendar_categories).forEach(async (category) => {
+        var events = parsed[category][month][day]
+        if (typeof events === 'undefined')
+        {
+            return
+        }
+        const calendarEmbed = new MessageEmbed()
         .setColor('#0099ff')
         .setTitle(event_types.calendar_categories[category])
         .setAuthor({ name: 'Beatrice the Seductress', iconURL: 'https://cdn.discordapp.com/avatars/951237146386251886/6365bdb68ecf9847880ea6e20e6ba05c.webp' })
@@ -38,15 +86,15 @@ const parseCalendar = async (message) => {
 
         Object.keys(events).forEach((gearScore) => {
             Object.keys(events[gearScore]).forEach((eventID) => {
-                console.log(categoryEmbed)
+                console.log(calendarEmbed)
                 if (typeof events[gearScore][eventID] !== 'undefined' && events[gearScore][eventID])
                 {
                     let event_time = JSON.stringify(events[gearScore][eventID]).slice(1, -1).replace(/['"]+/g, '')
-                    categoryEmbed.addField(event_types.calendar_events[eventID][0], event_time.replace(/\,/g,'\n'), false)
+                    calendarEmbed.addField(event_types.calendar_events[eventID][0], event_time.replace(/\,/g,'\n'), false)
                 }
             })
         })
-        await message.guild.channels.cache.find(i => i.name === 'bot-test').send({ embeds: [categoryEmbed] })
+        await message.guild.channels.cache.find(i => i.name === 'bot-test').send({ embeds: [calendarEmbed] })
     })
 
 }
